15f72d61a61c856c9498a553572b92410e465690
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-09-27 16:04:46 -0400

    build: about windows
    need to finish js to hide and show windows on click

---
+++ assets/js/tabs.js
@@ -0,0 +1,17 @@
+const about = document.getElementById('about')
+const certs = document.getElementById('certs')
+const contact = document.getElementById('contact')
+const photoGal = document.getElementById('photoGallery')
+
+const aboutWindow = document.getElementById('aboutWindow')
+const certWindow = document.getElementById('certWindow')
+const contWindow = document.getElementById('contactWindow') 
+const photoWindow = document.getElementById('photoGalleryWindow')
+
+about.addEventListener('click', 
+    console.log('Click')
+)
+
+certs.addEventListener('click', 
+    console.log('Click')
+)
