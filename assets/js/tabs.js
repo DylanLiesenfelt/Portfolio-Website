31958da0a3fad3da362415d542e1fb2598d4a09f
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-09-28 14:31:56 -0400

    build/fix: tab fuctions, about text fix
    Built the functionality for tabs to switch when corresponding button is clicked. fixed about text fonts resizing

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,17 +1,40 @@
-const about = document.getElementById('about')
-const certs = document.getElementById('certs')
-const contact = document.getElementById('contact')
-const photoGal = document.getElementById('photoGallery')
-
 const aboutWindow = document.getElementById('aboutWindow')
 const certWindow = document.getElementById('certWindow')
-const contWindow = document.getElementById('contactWindow') 
-const photoWindow = document.getElementById('photoGalleryWindow')
+const contactWindow = document.getElementById('contactWindow')
+const photoGalleryWindow = document.getElementById('photoGalleryWindow')
 
-about.addEventListener('click', 
-    console.log('Click')
+document.getElementById('about').addEventListener('click', function() {
+    aboutWindow.style.visibility = "visible";
+    contactWindow.style.visibility = "hidden";
+    certWindow.style.visibility = "hidden";
+    photoGalleryWindow.style.visibility = "hidden";
+    console.log('About Clicked')
+    }
 )
 
-certs.addEventListener('click', 
-    console.log('Click')
+document.getElementById('certs').addEventListener('click', function() {
+    aboutWindow.style.visibility = "hidden";
+    contactWindow.style.visibility = "hidden";
+    certWindow.style.visibility = "visible";
+    photoGalleryWindow.style.visibility = "hidden";
+    console.log('Certs Clicked')
+    }
 )
+
+document.getElementById('contact').addEventListener('click', function() {
+    aboutWindow.style.visibility = "hidden";
+    contactWindow.style.visibility = "visible";
+    certWindow.style.visibility = "hidden";
+    photoGalleryWindow.style.visibility = "hidden";
+    console.log('contact Clicked')
+    }
+)
+document.getElementById('photoGallery').addEventListener('click', function() {
+    aboutWindow.style.visibility = "hidden";
+    contactWindow.style.visibility = "hidden";
+    certWindow.style.visibility = "hidden";
+    photoGalleryWindow.style.visibility = "visible";
+    console.log('Gallery Clicked')
+    }
+)
+
