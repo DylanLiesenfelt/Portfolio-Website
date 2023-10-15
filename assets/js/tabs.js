31845b766bd5e08c98851c3e0bb974757470402f
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-10-15 00:39:20 -0400

    build: blog tab
    built blog tab and added a blog

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -2,12 +2,14 @@ const aboutWindow = document.getElementById('aboutWindow')
 const certWindow = document.getElementById('certWindow')
 const contactWindow = document.getElementById('contactWindow')
 const photoGalleryWindow = document.getElementById('photoGalleryWindow')
+const blogWindow = document.getElementById('blogWindow')
 
 document.getElementById('about').addEventListener('click', function() {
     aboutWindow.style.visibility = "visible";
     contactWindow.style.visibility = "hidden";
     certWindow.style.visibility = "hidden";
     photoGalleryWindow.style.visibility = "hidden";
+    blogWindow.style.visibility = "hidden"
     console.log('About Clicked')
     }
 )
@@ -17,6 +19,7 @@ document.getElementById('certs').addEventListener('click', function() {
     contactWindow.style.visibility = "hidden";
     certWindow.style.visibility = "visible";
     photoGalleryWindow.style.visibility = "hidden";
+    blogWindow.style.visibility = "hidden"
     console.log('Certs Clicked')
     }
 )
@@ -26,15 +29,28 @@ document.getElementById('contact').addEventListener('click', function() {
     contactWindow.style.visibility = "visible";
     certWindow.style.visibility = "hidden";
     photoGalleryWindow.style.visibility = "hidden";
+    blogWindow.style.visibility = "hidden"
     console.log('contact Clicked')
     }
 )
+
 document.getElementById('photoGallery').addEventListener('click', function() {
     aboutWindow.style.visibility = "hidden";
     contactWindow.style.visibility = "hidden";
     certWindow.style.visibility = "hidden";
     photoGalleryWindow.style.visibility = "visible";
+    blogWindow.style.visibility = "hidden"
     console.log('Gallery Clicked')
+    }
+)
+
+document.getElementById('blog').addEventListener('click', function() {
+    aboutWindow.style.visibility = "hidden";
+    contactWindow.style.visibility = "hidden";
+    certWindow.style.visibility = "hidden";
+    photoGalleryWindow.style.visibility = "hidden";
+    blogWindow.style.visibility = "visible"
+    console.log('Blog Clicked')
     }
 )
 
