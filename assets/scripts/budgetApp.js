656177d57b12bdde12bfe77903f9b0650bec7dc5
DylanLiesenfelt <dylanjames.code@gmail.com>
2024-02-09 01:51:59 -0500

    build: finance calculator

--- assets/scripts/budgetApp.js
+++ assets/scripts/budgetApp.js
@@ -1,11 +1,41 @@
-const incomeInput = document.getElementById("income-input");
-const expensesInput = document.getElementById("expenses-input");
-const budgetOutput = document. getElementById("budget-output");
+// Daily Adjusted Budget Calculator
+const DAB_Income = document.getElementById("DAB-income");
+const DAB_Expenses = document.getElementById("DAB-expenses");
+const DAB_Output = document. getElementById("DAB-output");
 
-function calculate() {
-    let income = parseFloat(incomeInput.value);
-    let expenses = parseFloat(expensesInput.value);
+function dailyAdjustedBudget() {
+    let income = parseFloat(DAB_Income.value);
+    let expenses = parseFloat(DAB_Expenses.value);
     let budget = (income - expenses)/30;
-    budgetOutput.innerText = '$ ' + budget.toFixed(2);
-    console.log(income, expenses, budget)
+    DAB_Output.innerText = '$ ' + budget.toFixed(2);
 }
+
+//Monthly Payment Calculator
+const MP_Principal = document.getElementById("MP-principal");
+const MP_Rate = document.getElementById("MP-rate");
+const MP_Months = document.getElementById("MP-months");
+const MP_Output = document.getElementById("MP-output");
+
+
+function monthlyPayment() {
+    let principal = MP_Principal.value;
+    let rate = MP_Rate.value / 100 / 12;
+    let months = MP_Months.value;
+    let output = principal * (rate * Math.pow(1 + rate, months)) / (Math.pow(1 + rate, months) - 1);
+    MP_Output.innerText = "$" + output.toFixed(2); 
+    console.log(principal, rate, months, output)
+}
+
+//Compound interest = p(1 + (r/n))^(nt)
+
+//Rule of 72 = 72/r aprox how long it will take to double an investment
+
+/*
+credit card payment calc 
+N = -(1/30) * (ln (1 + (b/p)(1-(1+i)^30))) / (ln (1+i))
+ln = nat log
+b = balance
+p = monthly payment
+i = daily interest = apr/365
+n= time itn takes to pay off
+*/
