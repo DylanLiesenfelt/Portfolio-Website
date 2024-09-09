146c48e0f7cf814dacf64d7b858a80c12bb810fd
Dylan Liesenfelt <dylanjames.code@gmail.com>
2024-09-08 22:14:23 -0400

    build fixes

--- assets/scripts/header.js
+++ assets/scripts/header.js
@@ -7,6 +7,7 @@ TAB.addEventListener('click', function() {
     if (isCollapsed) {
         NAV.style.width = '70px';
         NAV.style.visibility = 'visible';
+
     } else {
         NAV.style.width = '0px';
         NAV.style.visibility = 'hidden';
@@ -16,4 +17,32 @@ TAB.addEventListener('click', function() {
 });
 
 
+const ABOUT = document.getElementById('about')
+const PROJECTS = document.getElementById('projects')
+const RESUME = document.getElementById('resume')
+const ABOUTBTN = document.getElementById('about-btn')
+const PROJBTN = document.getElementById('proj-btn')
+const RESUMEBTN = document.getElementById('resume-btn')
+
+ABOUTBTN.addEventListener('click', function() {
+    ABOUT.style.visibility = 'visible'
+    PROJECTS.style.visibility = 'hidden'
+    RESUME.style.visibility = 'hidden'
+})
+
+PROJBTN.addEventListener('click', function() {
+    PROJECTS.style.visibility = 'visible'
+    ABOUT.style.visibility = 'hidden'
+    RESUME.style.visibility = 'hidden'
+})
+
+RESUMEBTN.addEventListener('click', function() {
+    RESUME.style.visibility = 'visible'
+    PROJECTS.style.visibility = 'hidden'
+    ABOUT.style.visibility = 'hidden'
+})
+
+let currentDate = Date.now
+
+console.log(currentDate)
 
